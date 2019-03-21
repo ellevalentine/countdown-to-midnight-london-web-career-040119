6a2130fafebfcef802b8a3c4925f40421800a2c3
Elle Valentine <elle.valentine@hotmail.com>
2019-03-21 14:17:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,12 +3,13 @@
 number = 10
 
 def countdown(number)
-  
   while number>0
   puts "#{number} SECOND(S)!"
   number-= 1
 end
-
 puts "HAPPY NEW YEAR!"
-  
 end
+
+def countdown_with_sleep(int)
+
+
