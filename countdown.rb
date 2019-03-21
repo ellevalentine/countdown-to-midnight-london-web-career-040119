6db42810f28b8bb8a3278919664870dfa484479f
Elle Valentine <elle.valentine@hotmail.com>
2019-03-21 14:21:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,9 @@
 #write your code here
 
-number = 10
+
 
 def countdown(number)
+  number = 10
   while number>0
   puts "#{number} SECOND(S)!"
   sleep(1.second)
@@ -12,10 +13,10 @@ puts "HAPPY NEW YEAR!"
 end
 
 
-int=5 
+
 
 def countdown_with_sleep(int)
-
+int=5 
  while int>0
   puts "#{int} SECOND(S)!"
   sleep 1
